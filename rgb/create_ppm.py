73a1d5c0907fa9f7b0ae4a77ea9279f6eeb90ecf
luisfelipe <luisito123_rocha@hotmail.com>
2024-10-10 11:29:56 -0300

    created an option to be able to scale the image

--- rgb/create_ppm.py
+++ rgb/create_ppm.py
@@ -1,19 +1,19 @@
 from random import randint
 
-def create_rgb_photo(intensity: int = 15): 
+def create_rgb_photo(intensity: int = 15, scale: int = 1): 
   with open(f"ppm_{intensity}.ppm", "w") as photo:
     photo_header(photo, intensity)
-    photo.write(photo_content(intensity))
+    photo.write(photo_content(intensity, scale))
 
 def photo_header(photo, intensity):
     photo.write(f"P3\n100 100\n {intensity}\n")
 
 
-def photo_content(intensity): 
+def photo_content(intensity, scale): 
   bytes = ""
   for _ in range(100):
-    bytes += " ".join(str(randint(0,intensity)) for _ in range(100 * 3))
+    bytes += " ".join(str(randint(0,intensity)) for _ in range(100 * 3 * scale))
 
   return bytes
 
-create_rgb_photo()
+create_rgb_photo(255, 10)
